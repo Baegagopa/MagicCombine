2c9a4dcdeafd04fa6388f9266c6b046a92fb5d52
MBDS <kbs99q@naver.com>
2017-03-13 21:00:24 +0900

    Start

---
+++ Assets/InterpretMagic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterpretMagic
+{
+    public float Plus(float a, float b)
+    {
+        return a + b;
+    }
+
+    public float Minus(float a, float b)
+    {
+        return a - b;
+    }
+
+    public float Division(float a, float b)
+    {
+        return a / b;
+    }
+
+    public float Multiply(float a, float b)
+    {
+        return a * b;
+    }
+
+    public float Involution(float a, float b)
+    {
+        return Mathf.Pow(a, b);
+    }
+
+    public float Sqrt(float a)
+    {
+        return Mathf.Sqrt(a);
+    }
+
+    public float GetStatus(ActorEnum actor, StatusEnum status)
+    {
+        switch (status)
+        {
+            case StatusEnum.Health:
+                return 10f;
+            case StatusEnum.Mana:
+                return 20f;
+            default:
+                return 0f;
+        }
+    }
+}
